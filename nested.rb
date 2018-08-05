63746c7447159759177d32b53231dc422e6954fb
Melissa Real <melissa.real@hotmail.com>
2018-08-05 18:22:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -135,9 +135,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].push("GUI")
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
-  programmer_hash
+     programmer_hash
 
 
 end
